8b19ccf62f56de865108b4a1c274327d24d96e56
NishanthJKumar <nishanth.kumar20@gmail.com>
2020-10-16 16:50:58 -0400

    Basic version of lmdb writer made!

--- utils/write_lmdb.py
+++ utils/write_lmdb.py
@@ -1,27 +1,71 @@
 import argparse
+import pdb
+import sys
+sys.path.remove('/home/nishanth/Desktop/RobotLearningBaselines/utils')
+import datetime
+from pympler import asizeof
+import lmdb
 import gym
 import rlbench.gym
 import os
-import sys
 import pickle
+import re
 import time
-import math
 from glob import glob
-import numpy as np
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from utils import *
+import numpy as np
 
 # IMPORTANT: Currently, this script is only intended to work for datasets including vision data
 # Do not use it on datasets that only take the 51D proprioceptive state.
+
+def key(x):
+    return int(x[x.rfind('/')+1:x.rfind('.')])
+
+def make_lmdb(mode, trajs):
+    # Setting the map_size of the lmdb to this database size doesn't work for some reason.
+    # No clue why. I picked a large default value to work so that things are fine for now...
+    # TODO: Figure out why the following 4 lines provides an UNDERESTIMATE of the lmdb size necessary
+    # object_size = asizeof.asizeof(pickle.load(open(expert_traj[0][0], mode='rb')))
+    # list_num_pickles = [len(traj) for traj in expert_traj]
+    # total_num_pickles = sum(list_num_pickles)
+    # database_size = total_num_pickles * (object_size + 10) # k,v pair stores approximately (object_size + 10 bytes)
+
+    lmdb_path = os.path.join(args.save_path, "{}.lmdb".format(mode))
+    isdir = os.path.isdir(lmdb_path)
+
+    with lmdb.open(lmdb_path, subdir=isdir, map_size=1099511627776 * 2,
+        readonly=False, meminit=False, map_async=True) as db:
+        txn = db.begin(write=True)
+        for path in trajs:
+            with open(path, mode='rb') as pickle_file:
+                expert_traj_data = pickle.load(pickle_file)
+                # Since the data collection setup saves files as '.../<trajectory>/<timestep>.pkl' for every trajectory
+                # we can recover this information to be used as the key for storing a trajectory data in the lmdb
+                m = re.search("(\d*)/(\d*).pkl",path)
+                trajectory = int(m.group(1))
+                timestep = int(m.group(2))
+                lmdb_key = (trajectory, timestep)
+                txn.put(str(lmdb_key).encode('utf-8'), pickle.dumps(expert_traj_data))
+    db.close()
+
+
 parser = argparse.ArgumentParser(description='Script to convert a dataset into an LMDB to enable efficient I/O and low memory usage')
 parser.add_argument('--raw-data-path', default="/tmp/rlbench_data", metavar='G',
                     help='path to the raw, expert trajectory data')
 parser.add_argument('--save-path', default="assets/lmdb_files", metavar='G',
-                    help='path at which to save the generated lmdb files')                    
+                    help='path at which to save the generated lmdb files')
+parser.add_argument('--train-test-split', default='0.9', metavar='G',
+                    help='fraction of trajectories to use for training and testing sets')                    
 args = parser.parse_args()
 
 # Make a directory at the correct save path
 os.makedirs(args.save_path)
 
-# TODO: loop through the files in the raw_data_path, then open, unpickle and read each one
-# and convert to an LMDB. Reference Jonathan's previous code if you need to
+expert_traj = [sorted(glob(paths+'/*'), key=key) for paths in glob(args.raw_data_path+'/*')]
+flattened_expert_traj = [timestep for trajectory in expert_traj for timestep in trajectory]
+total_num_timesteps = len(flattened_expert_traj)
+train_timesteps = round(total_num_timesteps * float(args.train_test_split))
+train_traj = flattened_expert_traj[:train_timesteps]
+test_traj = flattened_expert_traj[train_timesteps:]
+make_lmdb("train", train_traj)
+make_lmdb("test", test_traj)
